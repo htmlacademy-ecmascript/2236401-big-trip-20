0764898a899e2ea12795b3f2a64fa3c5d6cbe268
Yana Temirgalieva <kolesiko.yana@gmail.com>
2023-05-22 15:10:57 +0300

    making big changes (part 1)

--- src/model/waypoint-model.js
+++ src/model/waypoint-model.js
@@ -4,10 +4,18 @@ import { Offers, POINTS_COUNT } from '../const';
 
 export default class PointsModel {
   #points = createMockPoints(POINTS_COUNT);
-  offers = Offers;
-  destination = getRandomDestination();
+  #offers = Offers;
+  #destination = getRandomDestination();
 
   get points() {
     return this.#points;
+  }
+
+  get offers() {
+    return this.#offers;
+  }
+
+  get destination() {
+    return this.#destination;
   }
 }
---
+++ src/presenter/point-presenter.js
@@ -0,0 +1,119 @@
+import { render, replace, remove } from '../framework/render.js';
+import WaypointItemView from '../view/waypoint-item-view';
+import EventWaypointFormView from '../view/event-waypoint-form-view.js';
+import { isEscapeKey } from '../utils.js';
+
+const Mode = {
+  DEFAULT: 'DEFAULT',
+  EDITING: 'EDITING',
+};
+
+export default class PointPresenter {
+  #waypointListContainer = null;
+  #handleDataChange = null;
+  #handleModeChange = null;
+
+  #pointComponent = null;
+  #pointEditComponent = null;
+
+  #point = null;
+  #destinations = null;
+  #offers = null;
+  #mode = Mode.DEFAULT;
+
+  constructor ({ waypointListContainer, onDataChange, onModeChange }) {
+    this.#waypointListContainer = waypointListContainer;
+    this.#handleDataChange = onDataChange;
+    this.#handleModeChange = onModeChange;
+  }
+
+  init(point, destinations, offers) {
+    this.#point = point;
+    this.#destinations = destinations;
+    this.#offers = offers;
+
+    const prevPointComponent = this.#pointComponent;
+    const prevPointEditComponent = this.#pointEditComponent;
+
+    this.#pointComponent = new WaypointItemView({
+      point: this.#point,
+      destinations:this.#destinations,
+      offers:this.#offers,
+      onEditClick: this.#pointEditClickHandler,
+      onFavoriteClick: this.#handleFavoriteClick,
+    });
+    this.#pointEditComponent = new EventWaypointFormView({
+      point: this.#point,
+      destinations:this.#destinations,
+      offers:this.#offers,
+      onSubmit: this.#pointSubmitHandler,
+      onReset: this.#resetButtonClickHandler
+    });
+
+    if (prevPointComponent === null || prevPointEditComponent === null) {
+      render(this.#pointComponent, this.#waypointListContainer);
+      return;
+    }
+
+    if (this.#mode === Mode.DEFAULT) {
+      replace(this.#pointComponent, prevPointComponent);
+    }
+
+    if (this.#mode === Mode.EDITING) {
+      replace(this.#pointEditComponent, prevPointEditComponent);
+    }
+
+    remove(prevPointComponent);
+    remove(prevPointEditComponent);
+  }
+
+  destroy() {
+    remove(this.#pointComponent);
+    remove(this.#pointEditComponent);
+  }
+
+  resetView() {
+    if (this.#mode !== Mode.DEFAULT) {
+      this.#replaceFormToPoint();
+    }
+  }
+
+  #replacePointToForm = () => {
+    replace(this.#pointEditComponent, this.#pointComponent);
+    this.#handleModeChange();
+    this.#mode = Mode.EDITING;
+  };
+
+  #replaceFormToPoint = () => {
+    replace(this.#pointComponent, this.#pointEditComponent);
+    this.#mode = Mode.DEFAULT;
+  };
+
+  #escKeyDownHandler = (evt) => {
+    if (isEscapeKey) {
+      evt.preventDefault();
+      this.#replaceFormToPoint();
+      document.removeEventListener('keydown', this.#escKeyDownHandler);
+    }
+  };
+
+  #pointEditClickHandler = () => {
+    this.#replacePointToForm();
+    document.addEventListener('keydown', this.#escKeyDownHandler);
+  };
+
+  #resetButtonClickHandler = () => {
+    this.#replaceFormToPoint();
+    document.removeEventListener('keydown', this.#escKeyDownHandler);
+  };
+
+  #handleFavoriteClick = () => {
+    this.#handleDataChange({...this.#point, isFavorite: !this.#point.isFavorite});
+  };
+
+  #pointSubmitHandler = (point) => {
+    this.#handleDataChange(point);
+    this.#replaceFormToPoint();
+    document.removeEventListener('keydown', this.#escKeyDownHandler);
+  };
+}
